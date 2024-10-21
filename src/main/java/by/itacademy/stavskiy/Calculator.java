e3b311bd1ce5d22a3c04a3cda21aaebb1cd9fed1
tolbist4@gmail.com <AnatoliyStavskiy>
2024-10-21 16:14:42 +0300

    Corrected Exсeption

--- src/main/java/by/itacademy/stavskiy/Calculator.java
+++ src/main/java/by/itacademy/stavskiy/Calculator.java
@@ -2,28 +2,21 @@ package by.itacademy.stavskiy;
 
 public class Calculator {
 
-    public int sum(int a, int b){
+    public int sum(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b){
+    public int subtract(int a, int b) {
         return a - b;
     }
 
-    public int divide(int a, int b){
-        try {
-            if ( b == 0 ){
-                System.out.println("Делить на 0 нельзя");
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на 0");
-            throw new RuntimeException(e);
-        }
-
+    public int divide(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Делить на 0 нельзя");
         return a / b;
     }
 
-    public int multiply(int a, int b){
+    public int multiply(int a, int b) {
         return a * b;
     }
 }
